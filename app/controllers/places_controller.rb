910e125a12a90a6eb14077aa426385ac657dfcc2
Will Engellenner <will.engellenner@kellogg.northwestern.edu>
2023-05-17 15:40:32 +0000

    cont'd

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,4 +4,26 @@ class PlacesController < ApplicationController
     @places = Place.all
   end
 
+
+  def show
+    @place = Place.find_by({ "id" => params["id"] })
+    @post = Post.where({ "place_id" => params["id"] })
+  end
+  
+  def new
+    @place = Place.new
+  end
+    
+  def create
+    @place = Place.new
+
+    @place["name"] = params["place"]["name"]
+
+    @place.save
+
+    redirect_to "/places"
+
+  end
+
 end 
+
